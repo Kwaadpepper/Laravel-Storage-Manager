7201d73005e6c367808d9cfc2f727719987adcf9
Jérémy Munsch <github@jeremydev.ovh>
2026-07-19 10:39:11 +0200

    chore(deps): update eslint config with argsIgnorePattern and dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -31,6 +31,14 @@ export default [
                 'warn',
                 { allowConstantExport: true },
             ],
+            '@typescript-eslint/no-unused-vars': [
+                'error',
+                {
+                    argsIgnorePattern: '^_',
+                    varsIgnorePattern: '^_',
+                    destructuredArrayIgnorePattern: '^_',
+                },
+            ],
         },
     },
 ]
